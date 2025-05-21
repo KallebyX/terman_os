67e52a5408d3a9b371f138405e9c09f285555f69
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2025-05-21 11:52:53 -0300

    fix: corrigir caminho do módulo de configurações Django

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,10 +3,9 @@ import pytest
 from rest_framework_simplejwt.tokens import RefreshToken
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caminho_correto_do_seu_projeto.settings')
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
+
 def get_jwt_token():
     def _get_jwt_token(user):
         refresh = RefreshToken.for_user(user)
